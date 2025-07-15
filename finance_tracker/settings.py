c59ff6b519627fbbaef1e975317fda206741092e
C-Hammoud <chadi.h2000@gmail.com>
2025-07-16 00:08:58 +0300

    debud disabled

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-u^6gl(uq#257nm*r5w3qq-wq=j5muem$pnbp02@*gyg2tl$h5!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", os.environ.get("DJANGO_ALLOWED_HOSTS")]
